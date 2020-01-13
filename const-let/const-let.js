79741b97500a5c24ab79000da1ee5356217df255
starmanDX <jamiecordrey@gmail.com>
2020-01-12 22:13:08 -0800

    add const-let exercise files

---
+++ const-let/const-let.js
@@ -0,0 +1,11 @@
+//ES5
+// var name = 'Jane';
+// var title = 'Software Engineer';
+// var wage = 40;
+
+//ES6
+//const should not be expected to change
+//let should be expected to change
+const = 'Jane';
+let title = 'Software Engineer'; 
+let wage = 40;
